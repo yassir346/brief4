e3894b6244a9117028aa2947ac64ee1c5769a967
yassir346 <yassir.mahir98@gmail.com>
2024-11-01 12:30:59 +0100

    first commit

---
+++ mise_en_situation.js
@@ -0,0 +1,35 @@
+let Questions = [
+    {
+        question1 : "what is ...",
+        answer1 : "this is ...",
+    },
+
+    {
+        question2 : "what is ...",
+        answer2 : "this is ...",
+    },
+
+    {
+        question3 : "what is ...",
+        answer3 : "this is ...",
+    },
+
+    {
+        question4 : "what is ...",
+        answer4 : "this is ...",
+    },
+]
+
+
+
+function adding_q (){
+
+    quest1 = document.getElementById("question1");
+    ans1 = document.getElementById("question1");
+
+
+}
+
+function onfun() {
+    document.getElementById("question1") = ans1;
+  }
